9c8fe5a2692a57fda0b5c691146166513963e6ee
iiLsss <lish1993@163.com>
2023-05-15 14:41:50 +0800

    init

---
+++ src/components/Sider/index.tsx
@@ -0,0 +1,16 @@
+
+const Slider = () => {
+  return (
+    <div className='w-[260px] h-full p-3 border-r'>
+      <div className='w-full h-6'>
+        LOGO
+      </div>
+      <div className='w-full h-8 text-center drop-shadow-md'>
+        + New Chat
+      </div>
+      
+    </div>
+  )
+}
+
+export default Slider
