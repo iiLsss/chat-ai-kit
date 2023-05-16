7d9fad9304a5eca63f5a67edbfae7e22bf41a9d5
iiLsss <lish1993@163.com>
2023-05-16 19:19:28 +0800

    feat:

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,10 +1,19 @@
 'use client'
 
 import InputBox from '../InputBox'
+
 const Chat = () =>{
   return (
     <div className='flex flex-col items-center justify-between w-full h-full'>
-      <div></div>
+      <div className='flex-1 w-9/12 overflow-auto'>
+        {
+          Array.from({length: 100}, (_, index) => index + 1).map(item => (
+            <div key={item}>
+              {item}
+            </div>
+          ))
+        }
+      </div>
       <InputBox />
     </div>
   )
